7c327fcdd6857ff3f640ca9b1a4c242e6181736c
gongguan2018 <1542345123@qq.com>
2023-08-04 17:10:11 +0800

    one commit

--- pkg/conngrpc.go
+++ pkg/conngrpc.go
@@ -7,7 +7,7 @@ import (
 
 	"google.golang.org/grpc"
 )
-
+//连接grpc server
 func ConnRpcServer(cn *config.Config) (*grpc.ClientConn, error) {
 	//连接grpc-server，如果10s中没有连接成功,就视为连接失败
 	conn, err := grpc.Dial(fmt.Sprintf("%s:%d", cn.RpcServer.ServerIP, cn.RpcServer.RPCPort), grpc.WithInsecure(), grpc.WithTimeout(10*time.Second))
